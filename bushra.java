945e8ec0c33da8b430dc8235ed25126fbc8a4184
Bushramohammed <110836004+Bushramohammed@users.noreply.github.com>
2022-08-30 17:43:37 +0300

    Add files via upload

---
+++ bushra.java
@@ -0,0 +1,11 @@
+public class bushra {
+    public static void main(String[] args) {
+         System.out.println("    + ");
+        System.out.println("  +   + ");
+       System.out.println(" +     + ");
+       System.out.println("+-------+");
+       System.out.println("|  .-.  |");
+       System.out.println("|  | |  |");
+       System.out.println("+-+-+-+-+");
+    }
+}
---
+++ homework5.java
@@ -0,0 +1,8 @@
+public class homework5 {
+    public static void main(String[] args) {
+         System.out.println("__________");
+        System.out.println("| Bushra |");
+         System.out.println("__________");
+
+    }
+}
